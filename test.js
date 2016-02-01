dca5fc106e861c805733e51f6b1c5e3f3aa28825
Gregers Rygg <gregersrygg@gmail.com>
2016-02-01 16:36:45 +0100

    Remove unused function

--- test.js
+++ test.js
@@ -77,15 +77,6 @@ function arrayContains (arr, value) {
     return arr.indexOf(value) !== -1;
 }
 
-function toArrayBuffer(buffer) {
-    var ab = new ArrayBuffer(buffer.length);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buffer.length; ++i) {
-        view[i] = buffer[i];
-    }
-    return view;
-}
-
 describe('maven-deploy', function () {
     beforeEach(function () {
         lastCmd = undefined;
